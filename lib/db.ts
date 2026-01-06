80af97f1960c944adf3df1e7fdf9cf23c773cc57
tasawr01 <tasawarhussain33831@gmail.com>
2026-01-07 02:26:08 +0500

    Version Error Resolve

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
@@ -31,12 +29,16 @@ async function dbConnect() {
         return cached.conn;
     }
 
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
